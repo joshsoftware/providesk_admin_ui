a6685c1a1546d5f5bf54d7bd4b519d8618f38b10
Rupesh Gadekar <rupesh.gadekar@joshsoftware.com>
2022-12-02 15:58:02 +0530

    StyledErrorText Changes

--- src/modules/shared/StyledErrorText.tsx
+++ src/modules/shared/StyledErrorText.tsx
@@ -2,16 +2,7 @@ import { Typography } from '@mui/material';
 export const StyledErrorText = ({ text, sx }: { text: string; sx?: any }) => {
   return (
     <>
-      <Typography
-        variant='caption'
-        sx={{
-          fontSize: '11px',
-          color: '#f44336',
-          display: 'flex',
-          alignItems: 'center',
-          ...sx,
-        }}
-      >
+      <Typography variant='body1' component='p' sx={{color: 'error.main', p: '0.125rem 0.875rem 0 0.875rem', ...sx}}>
         {text}
       </Typography>
     </>
