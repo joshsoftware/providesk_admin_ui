837621b8eee8ac0ff88fffd453b7ecd3c496dff3
ayushjnv1 <ayushjnv1@gmail.com>
2023-01-05 12:39:00 +0530

    add role

--- src/modules/Users/users.helpers.ts
+++ src/modules/Users/users.helpers.ts
@@ -3,7 +3,9 @@ import { ROLES } from 'routes/roleConstants';
 export const getAllowedRoles = (role) => {
   let allowedRoles;
   if (role === ROLES.ADMIN) {
-    allowedRoles = ['Employee', 'Department_Head', 'Admin'];
+    allowedRoles = ['Employee', 'Department_Head', 'Admin', 'Resolver'];
+  } else if (role === ROLES.DEPARTMENT_HEAD) {
+    allowedRoles = ['Employee', 'Department_Head', 'Resolver'];
   } else {
     allowedRoles = ['Employee'];
   }
